7109c3f366a9af6f42bc04bca09243935e116ac2
afantiniv <fantiniv95@gmail.com>
2022-12-26 01:07:38 +0000

    Todo list API listo

--- src/api/models.py
+++ src/api/models.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__="user"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
@@ -16,4 +17,22 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Todos(db.Model):
+    __tablename__="todos"
+    id=db.Column(db.Integer,primary_key=True)
+    label=db.Column(db.String(150), nullable=False)
+    done=db.Column(db.Boolean(), nullable=False)
+    user_id=db.Column(db.Integer, db.ForeignKey("user.id"))
+    user=db.relationship(User)
+
+    def __repr__(self):
+        return '<Todos %r>' % self.id
+    
+    def serialize(self):
+        return {
+            "label":self.label,
+            "done":self.done,
+            "user_id":self.user_id
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,8 +1,8 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from flask import Flask, request, jsonify, url_for, Blueprint, jsonify
+from api.models import db, User, Todos
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,43 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/todos/<int:user_param>', methods=['GET'])
+def get_todos(user_param):
+    todos=Todos.query.filter(Todos.user_id==user_param).all()
+    user_data=todos[0].user.serialize()
+    return jsonify({
+        "user":user_data,
+        "todos":list(map(lambda item: item.serialize(),todos))
+    }), 200
+
+@api.route('/todos/<int:user_param>', methods=['POST'])
+def post_todo(user_param):
+    label=request.json.get("label")
+    done=request.json.get("done")
+    newTodo=Todos(user_id=user_param, label=label, done=done)
+    db.session.add(newTodo)
+    db.session.commit()
+
+    return jsonify({"resp":"Todo creado con exito"}), 201
+
+@api.route('/todos/<int:user_param>/<int:todo_index>', methods=['PUT'])
+def update_todo(user_param, todo_index):
+    label=request.json.get("label")
+    done=request.json.get("done")
+    todos=Todos.query.filter(Todos.user_id==user_param).all()
+    newTodo=todos[todo_index]
+    newTodo.label=label
+    newTodo.done=done
+    db.session.add(newTodo)
+    db.session.commit()
+    return jsonify({"resp":"Todo actualizado con exito"}), 200
+
+@api.route('/todos/<int:user_param>/<int:todo_index>', methods=['DELETE'])
+def delete_todo(user_param, todo_index):
+    todos=Todos.query.filter(Todos.user_id==user_param).all()
+    todo_delete=todos[todo_index]
+    db.session.delete(todo_delete)
+    db.session.commit()
+    return jsonify({"resp":"Todo eliminado con exito"}), 200
+
